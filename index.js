b9b6508921bd4c35fb3c190622746111b5ce11d3
your_name_here <cssprasanth99@gmail.com>
2024-08-03 22:28:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // Use your email service
+    auth: {
+      user: "cssprasanth99@gmail.com", // Your email
+      pass: "zbqz qjkr nqek nooy", // Your email password
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: "cssprasanth99@gmail.com",
+    subject: `Contact Form Submission from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send("Email sent: " + info.response);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
